f87a4de5f94a5fcf82188fafb1d25b636613aa76
Roman Yarovoi <roman.and.yarovoi@gmail.com>
2019-12-07 18:30:59 +0200

    change worker, position

--- employee/admin.py
+++ employee/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Workers
+from .models import Workers, Position
 
 # Register your models here.
 admin.site.register(Workers)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- employee/models.py
+++ employee/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField('Название', max_length=50)
+    slug = models.SlugField('Slug', max_length=50)
+
+    def __str__(self):
+        return self.name 
 
 
 class Workers(models.Model):
-    # works_position = ('Работник зала', 'Работник кухни ', 'Админестратор')
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    Position = (
-        ('Работники зала', 'Работники зала'),
-        ('Работники кухни', 'Работники кухни'),
-        ('Управляющий персонал', 'Управляющий персонал'),
-    )
-    position = models.CharField(max_length=50,
-                                choices=Position)
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name='workers', blank=True, null=True)
     phone = models.IntegerField()
     address = models.CharField(max_length=100)
 
