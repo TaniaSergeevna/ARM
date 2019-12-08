03e83fde460629c1a4e8d4a27b3ddc77b61a1252
tatiana <the_wicked_with@ukr.net>
2019-12-08 15:48:21 +0200

    new migration

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from menu.models import Menu
-
+from menu.models import Menu, Sections
 
 admin.site.register(Menu)
+
+
+@admin.register(Sections)
+class Section_admin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- menu/models.py
+++ menu/models.py
@@ -1,19 +1,28 @@
 from django.db import models
 
+class Sections(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Menu(models.Model):
-    MENU_SECTION = (('Закуски', 'Закуски'),
-                    ('Салаты', 'Салаты'),
-                    ('Гарниры', 'Гарниры'),
-                    ('Мясо', 'Мясо'),
-                    ('Десерты', 'Десерты'),
-                    ('Напитки', 'Напитки'),
-                    ('Алкоголь', 'Алкоголь'),
-                    )
+    # MENU_SECTION = (('Закуски', 'Закуски'),
+    #                 ('Салаты', 'Салаты'),
+    #                 ('Гарниры', 'Гарниры'),
+    #                 ('Мясо', 'Мясо'),
+    #                 ('Десерты', 'Десерты'),
+    #                 ('Напитки', 'Напитки'),
+    #                 ('Алкоголь', 'Алкоголь'),
+    #                 )
+    sections = models.ForeignKey(Sections, on_delete=models.CASCADE, related_name='menus', blank=True,
+                                 null=True)
     name = models.CharField(max_length=100)
-    menu_section = models.CharField(max_length=100, choices=MENU_SECTION)
+    # menu_section = models.CharField(max_length=100, choices=MENU_SECTION)
     weight = models.IntegerField()
     price = models.IntegerField()
 
     def __str__(self):
         return self.name
+
